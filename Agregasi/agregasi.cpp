32da3ec2df6dbb93648879e921663b83e36c9701
fadilsyy <fadilasytar@gmail.com>
2025-05-27 18:47:54 +0700

    program selesai

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -5,8 +5,7 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main()
-{
+int main() {
     ibu* varIbu = new ibu("dini");
     ibu* varIbu2 = new ibu("novi");
     anak* varAnak1 = new anak("tono");
