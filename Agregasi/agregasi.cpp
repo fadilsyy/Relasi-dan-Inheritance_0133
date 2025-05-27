5a79233c319eca6f8f8ac44490db832dd3e3ce0d
fadilsyy <fadilasytar@gmail.com>
2025-05-27 18:27:16 +0700

    pembuatan main

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #include "anak.h"
@@ -6,5 +7,14 @@ using namespace std;
 
 int main()
 {
-    
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
 }
