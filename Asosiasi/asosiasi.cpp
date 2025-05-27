aa1bc293ce3f5d2a7ef09e361cca2ccdc910a746
fadilsyy <fadilasytar@gmail.com>
2025-05-27 19:02:27 +0700

    program selesai

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -49,6 +49,38 @@ void pasien::cetakDokter() {
     cout << endl;
 }
 
-int main() {
+void dokter::tambahPasien(pasien* pPasien) {
+    cout << "Daftar pasien dari dokter \""
+    << this->nama << "\":\n";
+    for(auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
 
+int main() {
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varPasien1->tambahDokter(varDokter1);
+    varPasien2->tambahDokter(varDokter1);
+    varPasien1->tambahDokter(varDokter2);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
 }
