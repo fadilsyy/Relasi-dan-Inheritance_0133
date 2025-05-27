4269a58017c9c6f5ab3ce67f9a876e5d6196b7a2
fadilsyy <fadilasytar@gmail.com>
2025-05-27 18:53:09 +0700

    pembuatan prosedur

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -14,7 +14,7 @@ class pasien{
             cout << "Pasien \"" << nama
             << "\" tidak ada\n";
         }
-        void tambahDokter(dokter);
+        void tambahDokter(dokter*);
         void cetakDokter();
 };
 
@@ -30,7 +30,24 @@ class dokter {
             cout << "Dokter \"" << nama
             << "\" tidak ada\n";
         }
+        void tambahPasien(pasien*);
+        void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \""
+    << this->nama << "\":\n";
+    // auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
 
 int main() {
 
