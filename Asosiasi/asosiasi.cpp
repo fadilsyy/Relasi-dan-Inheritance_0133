9831daa713383272009d927750e8800b986042a9
fadilsyy <fadilasytar@gmail.com>
2025-05-27 19:12:09 +0700

    program selesai

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -50,6 +50,10 @@ void pasien::cetakDokter() {
 }
 
 void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back (pPasien);
+}
+
+void dokter::cetakPasien() {
     cout << "Daftar pasien dari dokter \""
     << this->nama << "\":\n";
     for(auto& a : daftar_pasien) {
